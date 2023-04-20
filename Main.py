1f1c65b78e02964347441b2520d9aaf928e10603
Caleb P. Nwokocha <nwo20001@byui.edu>
2023-04-20 06:05:40 -0500

    Testing

--- Main.py
+++ Main.py
@@ -92,7 +92,7 @@ if __name__ == "__main__":
     
 
     # Uncomment the code below to see the two nodes preformance on prime number prediction test.
-    
+    """
     # To train the two nodes for predicting prime numbers
     # Create input and target datasets for the first 100 prime numbers
     training_inputs = [float(i) for i in range(1, 101)]
@@ -137,4 +137,4 @@ if __name__ == "__main__":
     print(f"Node 2: Mean Squared Error = {mse2}")
     print()
     print("TESTING COMPLETE")
-    
+    """
